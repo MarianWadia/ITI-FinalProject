5c065b463c5795e362a35a602808aa2a26a6e808
kariem atef <katef4421@gmail.com>
2023-09-23 06:32:36 +0300

    Comment The previos index to the orignal file , you should access api from routes file

--- backend/index.js
+++ backend/index.js
@@ -1,83 +1,105 @@
 
-const express = require('express')
-var mongoose = require('mongoose')
-var Clothes = require('./models/ClothModel');
-var cors = require('cors');
-const exp = require('constants');
-var server = express()
+// const express = require('express')
+// var mongoose = require('mongoose')
+// var Clothes = require('./models/ClothModel');
+// var cors = require('cors');
+// const exp = require('constants');
+// var server = express()
 
-server.use(express.urlencoded({extended:true}))
+// server.use(express.urlencoded({extended:true}))
 
-server.use(express.json())
-server.use(cors())
-//connect to DataBase
-mongoose.connect('..link database..')
-.then(()=>{
-    console.log("Connected to database");
-})
-.catch((err)=>{
-    console.log("error connecttion");
-});
-//All Products
-server.get('/products',function(req,res){
+// server.use(express.json())
+// server.use(cors())
+// //connect to DataBase
+// mongoose.connect('..link database..')
+// .then(()=>{
+//     console.log("Connected to database");
+// })
+// .catch((err)=>{
+//     console.log("error connecttion");
+// });
+// //All Products
+// server.get('/products',function(req,res){
     
-    Clothes.find()
-    .then((productData)=>{
-        console.log(productData)
-        res.send(productData)
-    })
-    .catch((err)=>{
-        res.send({
-            error:'Error getting product'
-        })
-    })
-});
+//     Clothes.find()
+//     .then((productData)=>{
+//         console.log(productData)
+//         res.send(productData)
+//     })
+//     .catch((err)=>{
+//         res.send({
+//             error:'Error getting product'
+//         })
+//     })
+// });
 
-//ADD Product 
-server.post("/products", function (req, res) {
-    let productData = req.body;
-    let newProduct = new Clothes({
-      title: productData.title,
-      price: +productData.price,
-      description: productData.description,
-      category: productData.category,
-      image: productData.image,
-    });
+// //ADD Product 
+// server.post("/products", function (req, res) {
+//     let productData = req.body;
+//     let newProduct = new Clothes({
+//       title: productData.title,
+//       price: +productData.price,
+//       description: productData.description,
+//       category: productData.category,
+//       image: productData.image,
+//     });
   
-    newProduct
-      .save()
-      .then((msg) => {
-        console.log("ADD SUCCESSFULLY");
-        res.send({
-          msg: "product added successfully",
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).send({
-          error: "Error adding product",
-        });
-      });
-  });
+//     newProduct
+//       .save()
+//       .then((msg) => {
+//         console.log("ADD SUCCESSFULLY");
+//         res.send({
+//           msg: "product added successfully",
+//         });
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//         res.status(500).send({
+//           error: "Error adding product",
+//         });
+//       });
+//   });
   
-//Delte Product 
-server.delete("/products/:id",function(req,res){
+// //Delte Product 
+// server.delete("/products/:id",function(req,res){
    
-  let prodId = +req.params.id;
-  Clothes.deleteOne(
-      {id:prodId}
-  ).then((msg)=>{
-    console.log("Delete Successfully")
-      res.send({
+//   let prodId = +req.params.id;
+//   Clothes.deleteOne(
+//       {id:prodId}
+//   ).then((msg)=>{
+//     console.log("Delete Successfully")
+//       res.send({
         
-          msg: "product Deleted successfully"
-      })
-  }).catch((err)=>{
-      console.log(err)
-  })
+//           msg: "product Deleted successfully"
+//       })
+//   }).catch((err)=>{
+//       console.log(err)
+//   })
 
-})
+// })
 
-server.listen(3002,function(){
-    console.log("Server connected");
+// server.listen(3002,function(){
+//     console.log("Server connected");
+// })
+
+
+
+
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors');
+const clothesRoute = require('./routes/ClothesRoute')
+const userRoute=require('./routes/UsersRoute')
+const app = express();
+mongoose.connect('mongodb://127.0.0.1:27017/ItiClothes')
+app.use(express.json());
+app.use(clothesRoute)
+app.use(userRoute)
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log('listening')
 })
